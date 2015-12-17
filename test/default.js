2e7572a01b6f2556438b39282d0563781a838341
em <michael@joinbox.com>
2015-12-18 00:09:08 +0100

    added somemore compils & tests

--- test/default.js
+++ test/default.js
@@ -20,17 +20,100 @@
 
 
         it('should compile a native module', function(done) {
-            let context = new QueryContext({
-                ast: {
-                      kind: 'createDatabase'
-                    , databaseName: 'test'
-                }
-            });
+            let context = new QueryContext({ast: {
+                  kind: 'createDatabase'
+                , database: 'test'
+            }});
 
             new VendorCompiler().compile(context).then(() => {
-                assert('CREATE DATABASE "test";' === context.sql);
+                assert.equal('CREATE DATABASE "test";', context.sql);
                 done();
             }).catch(done);
         });
-	});  
+	});
+
+
+
+
+
+
+
+    describe('Native Compilers', function(){
+        it('createDatabase', function(done) {
+            let context = new QueryContext({ast: {
+                  kind: 'createDatabase'
+                , database: 'testDatabase'
+            }});
+
+            new VendorCompiler().compile(context).then(() => {
+                assert.equal('CREATE DATABASE "testDatabase";', context.sql);
+                done();
+            }).catch(done);
+        });
+
+
+        it('select', function(done) {
+            let context = new QueryContext({ast: {
+                  kind: 'select'
+                , selection: ['id', 'c', 'id', 'b']
+            }});
+
+            new VendorCompiler().compile(context).then(() => {
+                assert.equal('SELECT "id", "c", "b"', context.sql);
+                done();
+            }).catch(done);
+        });
+
+
+        it('from', function(done) {
+            let context = new QueryContext({ast: {
+                  kind: 'from'
+                , entity: 'testEntitiy'
+                , database: 'testDatabase'
+            }});
+
+            new VendorCompiler().compile(context).then(() => {
+                assert.equal('FROM "testDatabase"."testEntitiy"', context.sql);
+                done();
+            }).catch(done);
+        });
+
+
+        it('function', function(done) {
+            let context = new QueryContext({ast: {
+                  kind: 'function'
+                , name: 'doSomethingUseful'
+                , parameters: ['a', 1]
+            }});
+
+            new VendorCompiler().compile(context).then(() => {
+                assert.equal(`"doSomethingUseful"('a', '1')`, context.sql);
+                done();
+            }).catch(done);
+        });
+
+
+        it('selectQuery', function(done) {
+            let context = new QueryContext({ast: {
+                  kind: 'selectQuery'
+                , select: {
+                      kind: 'select'
+                    , selection: ['*']
+                }
+                , from: {
+                      kind: 'from'
+                    , entity: {
+                          kind: 'function'
+                        , name: 'doSomethingUseful'
+                        , parameters: ['a', 1]
+                    }
+                }
+            }});
+
+            new VendorCompiler().compile(context).then(() => {
+                assert.equal(`SELECT "*" FROM "doSomethingUseful"('a', '1');`, context.sql);
+                done();
+            }).catch(done);
+        });
+    });
 })();
